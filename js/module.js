993c02d9cb31272cdda18fdd0c1d2347f9a3637b
jtabach <jeff.n.tabachnick@gmail.com>
2016-02-23 17:59:48 -0800

    portfolio cards

--- js/module.js
+++ js/module.js
@@ -3,3 +3,7 @@ var app = angular.module('myApp', ['ngMaterial']);
 app.controller('mainCtrl', function($scope) {
 	console.log('main')
 });
+
+app.controller('portfolioCtrl', function($scope) {
+  $scope.imagePath = '/images/pa.png';
+});
