c2fbd06edeaac62ca414cae5d1a7ea68af182944
jtabach <jeff.n.tabachnick@gmail.com>
2016-03-18 22:18:41 -0700

    add twitter posts

--- js/module.js
+++ js/module.js
@@ -4,6 +4,7 @@ app.value('duScrollDuration', 1500);
 
 app.controller('mainCtrl', function($scope) {
 	console.log('main');
+	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 });
 
 app.controller('portfolioCtrl', function($scope) {
